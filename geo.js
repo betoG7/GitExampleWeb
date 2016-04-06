f80d8ccdbc028f289724009d393e529eee75b0b5
betoG7 <humbertogarciag7@gmail.com>
2016-04-06 16:14:58 -0500

    Clase de Ajax

---
+++ geo.js
@@ -0,0 +1,28 @@
+$(function(){
+	var geo = navigator.geolocation;
+	var opciones = {};
+	console.log(geo);
+
+	function geo_error()
+	{
+		console.log("No se puede compi");
+	}
+	function geo_exito(posicion)
+	{
+		console.log(posicion);
+		var lat = posicion.coords.latitude;
+		var lon = posicion.coords.longitude;
+
+		var mapa = new Image();
+		mapa.src = "http://maps.googleapis.com/maps/api/staticmap?zoom=13&size=200x200&sensor=false&center="+lat+","+lon;
+
+		$('#geo').append(mapa);
+	}
+
+
+
+	geo.getCurrentPosition(geo_exito, geo_error, opciones);
+
+
+	
+})
